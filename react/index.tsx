0cea85b3298a593dcb89c6bb8913e542c971189b
Breno Calazans <brenocalazans@gmail.com>
2020-03-30 18:45:17 -0300

    Add initial implementation

--- react/index.tsx
+++ react/index.tsx
@@ -1,10 +1,66 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { canUseDOM } from 'vtex.render-runtime'
 import { PixelMessage } from './typings/events'
+import { getCountryISO2 } from './modules/iso-3-to-2'
+
+function addScript() {
+  const script = document.createElement('script')
+  script.async = true
+  script.defer = true
+  script.src = 'https://apis.google.com/js/platform.js?onload=renderOptIn'
+  document.body.appendChild(script)
+}
 
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
-    case 'vtex:pageView': {
+    case 'vtex:orderPlacedTracked':
+    case 'vtex:orderPlaced': {
+      const data = e.data
+      const merchantId = window.__google_customer_reviews.merchantId
+      const passGtin = window.__google_customer_reviews.passGtin
+      const optInStyle = window.__google_customer_reviews.optInStyle
+      const showBadge = window.__google_customer_reviews.showBadge
+      const badgePosition = window.__google_customer_reviews.badgePosition
 
+      const shippingEstimateDate = new Date()
+      const estimate = shippingEstimateDate.toISOString().slice(0, 10)
+
+      window.renderOptIn = function() {
+        if (showBadge === 'true') {
+          const ratingBadgeContainer = document.createElement('div')
+          document.body.appendChild(ratingBadgeContainer)
+
+          window.gapi.load('ratingbadge', function() {
+            window.gapi.ratingbadge.render(ratingBadgeContainer, {
+              merchant_id: merchantId,
+              position: badgePosition,
+            })
+          })
+        }
+
+        window.gapi.load('surveyoptin', function() {
+          window.gapi.surveyoptin.render({
+            // REQUIRED
+            merchant_id: merchantId,
+            order_id: data.orderGroup,
+            email: data.visitorContactInfo[0],
+            delivery_country: getCountryISO2(
+              data.visitorAddressCountry as keyof typeof getCountryISO2
+            ),
+            estimated_delivery_date: estimate,
+            // OPTIONAL
+            products:
+              passGtin === 'true'
+                ? data.transactionProducts.map(product => ({
+                    gtin: product.ean,
+                  }))
+                : undefined,
+            opt_in_style: optInStyle,
+          })
+        })
+      }
+
+      addScript()
     }
     default: {
       return
