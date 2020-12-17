a89763b8e7e5db7f0df8d552267e19b35944115e
Breno Calazans <brenocalazans@gmail.com>
2020-12-17 16:22:36 -0300

    Add support for inline badge block

--- react/Badge.tsx
+++ react/Badge.tsx
@@ -1,20 +1,37 @@
-import React, { FC } from 'react'
+import React, { useRef } from 'react'
+import { useRuntime } from 'vtex.render-runtime'
 
-const Badge: FC = () => {
+function Badge() {
+  const {
+    culture: { language },
+  } = useRuntime()
+
+  const languageSet = useRef<boolean>(false)
+
   const merchantId = window.__google_customer_reviews
     ? window.__google_customer_reviews.merchantId
     : ''
+
+  if (!languageSet || languageSet.current === false) {
+    // @ts-expect-error as expected by their docs https://support.google.com/merchants/answer/7105655?hl=en&ref_topic=7105160
+    window.___gcfg = { lang: language }
+    languageSet.current = true
+  }
 
   if (!merchantId) {
     return null
   }
 
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `<g:ratingbadge merchant_id=${merchantId}></g:ratingbadge>​`,
-      }}
-    ></div>
+    <>
+      <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `<g:ratingbadge merchant_id=${merchantId}></g:ratingbadge>​`,
+        }}
+      ></div>
+    </>
   )
 }
 
